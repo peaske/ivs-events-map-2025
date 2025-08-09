d6d0cf20ca69fad4d95942b70f90838ad41a65d1
Peaske Kawahara <peaske@Peaskes-MacBook-Air.local>
2025-08-09 12:27:42 +0900

    🔄 GitHub Actions設定に変更完了

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ivs-events-map-2025/'
+  base: '/ivs-events-map-2025/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'google-maps': ['@googlemaps/js-api-loader']
+        }
+      }
+    }
+  },
+  publicDir: 'public'
 })
